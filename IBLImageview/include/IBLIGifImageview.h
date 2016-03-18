254aeecbec12106558a6895077d1e9ddec777f55
simpossible <ljfsteven@163.com>
2016-03-18 17:58:45 +0800

    新建工程

---
+++ IBLImageview/include/IBLIGifImageview.h
@@ -0,0 +1,15 @@
+//
+//  IBLIGifImageview.h
+//  IBLImageview
+//
+//  Created by simpossible on 16/3/18.
+//  Copyright © 2016年 simpossible. All rights reserved.
+//
+
+#ifndef IBLIGifImageview_h
+#define IBLIGifImageview_h
+
+#import "IBLImageView.h"
+#import "IBLImage.h"
+
+#endif /* IBLIGifImageview_h */
